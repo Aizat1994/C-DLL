2906f9abbe797192122134cf9cfc41895d6ba95a
garifullin aizat <garifullinaizat@gmail.com>
2022-12-13 16:27:20 +0600

    first commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace ConsoleApp3
+{
+    class Program
+    {
+     
+        public const string CppFunctionDll = @"C:\\Users\\assai\\source\\repos\\NewRepo6\\Debug\\Dll1.dll";
+
+        [DllImport(CppFunctionDll)]
+        public static extern double sumTwo(double var_x, double var_y);
+
+        static void Main()
+        {
+            double var_x = 10;
+            double var_y = 5;
+
+            double result = sumTwo(var_x, var_y);
+            Console.WriteLine(result);
+        }
+      
+    }
+}
+
